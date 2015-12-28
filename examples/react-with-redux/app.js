e428932321f42e282c1261bc1dcbc33e96c179ad
casperchen(陈映平) <chyingp@gmail.com>
2015-12-28 21:55:43 +0800

    提交createStore的源码解析

---
+++ examples/react-with-redux/app.js
@@ -0,0 +1,77 @@
+var Redux = require('redux');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// action creator
+var addTodoActions = function(text){
+	return {
+		type: 'add_todo',
+		text: text
+	};
+};
+
+// reducers
+var todoReducer = function(state, action){
+	
+	if(typeof state === 'undefined'){
+		return [];
+	}
+	
+	switch(action.type){
+		case 'add_todo':
+			return state.slice(0).concat({
+				text: action.text,
+				completed: false
+			});
+			break;
+		default:
+			return state;
+	}
+};
+
+
+var store = Redux.createStore(todoReducer);
+
+var App = React.createClass({
+	getInitialState: function(){
+		return {
+			items: store.getState()
+		};
+	},
+	componentDidMount: function(){
+		var unsubscribe = store.subscribe(this.onChange);
+	},
+	onChange: function(){
+		this.setState({
+			items: store.getState()
+		});
+	},
+	handleAdd: function(){
+		var input = ReactDOM.findDOMNode(this.refs.todo);
+		var value = input.value.trim();
+
+		if(value)
+			store.dispatch(addTodoActions(value));
+
+		input.value = '';
+	},
+	render: function(){
+		return (
+			<div>
+				<input ref="todo" type="text" placeholder="输入todo项" style={{marginRight:'10px'}} />
+				<button onClick={this.handleAdd}>点击添加</button>
+				<ul>
+					{this.state.items.map(function(item){
+						return <li>{item.text}</li>;
+					})}
+				</ul>
+			</div>			
+			);
+	}
+});
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('container')
+	);
+
